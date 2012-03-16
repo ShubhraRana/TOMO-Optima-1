502a70118e382eb84f991fa924f8e8f11321bd60
shubhra199028 <shubhra199028@gmail.com>
2012-03-16 18:03:39 +0530

    third commit

--- temp/tree.cpp
+++ temp/tree.cpp
@@ -1,51 +1,51 @@
+#include <math.h>
 #include<iostream>
 using namespace std;
-
-class tree;
-
-class node 
+//int level[20]={0};
+class element
 {
-int value;
-node *left,*right,*peer;
-public:
-node()
+private : int val;
+         // bin_tree* left;
+        //  bin_tree* right;
+          
+public :
+      void set(int num)
+        {
+        val=num;
+        }
+
+     int get(){
+     return(val);
+    }
+
+}nodes[100];
+
+void display(int n){
+        cout<<"\n";
+        int d=1;
+        for(int i=0;i<n;i++)
+        {
+           if((i+1)==pow(2,d))
+           { cout<<"\n";d+=1;}
+           cout<<nodes[i].get()<<"\t";
+           
+        }
+       }
+ main()
 {
-  left = NULL;
-  right = NULL;
-  peer = NULL;
+int i;
+for(i=0;i<100;i++)
+nodes[i].set(-5);
+
+//creation of nodes
+int n,num;
+cout<<"Enter the number of nodes in the tree: ";
+cin>>n;
+for(i=0;i<n;i++)
+{
+ cout<<"enter the value of "<<i+1<<" node:";
+ cin>>num;
+ nodes[i].set(num);
 }
-};
-
-class tree
-{
-node *root;
-public:
-tree()
-{
-  root = NULL;
+display(n);
 }
-
-node* go_to_node(node *x, int flag)
-{
-  if(x == NULL)
-    return x;
-  if(flag == 0)
-   return x;
-  x = go_to_node(x->left,1);
- 
-}
-
-void add_node(int val)
-{
-  if(root == NULL)
-  {
-    root = new node();
-    root->value = val;
-    return; 
-  }
-  // if root is not empty
-  node *temp = go_to_node(root,1);
- 
-}
-
-};
