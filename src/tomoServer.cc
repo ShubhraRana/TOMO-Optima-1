ee3407be738976bbf84e0902cf0cdb427ceabfcc
quakerr90 <sidharthamani@Sidhartha-Manis-MacBook-Pro.local>
2012-03-18 16:49:22 +0530

    origin

--- src/tomoServer.cc
+++ src/tomoServer.cc
@@ -1,15 +1,35 @@
-#include<iostream>
-#include<cstdlib>
-#include<ns3/core-module.h>
-#include<ns3/internet-module.h>
-#include<ns3/network-module.h>
-#include<deque>
+#include "iostream"
+#include "cstdlib"
+#include "ns3/core-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/network-module.h"
+#include "deque"
+#include "ns3/application.h"
+#include "ns3/event-id.h"
+#include "ns3/ptr.h"
+#include "ns3/address.h"
+#include "ns3/log.h"
+#include "ns3/ipv4-address.h"
+#include "ns3/address-utils.h"
+#include "ns3/nstime.h"
+#include "ns3/inet-socket-address.h"
+#include "ns3/socket.h"
+#include "ns3/udp-socket.h"
+#include "ns3/simulator.h"
+#include "ns3/socket-factory.h"
+#include "ns3/packet.h"
+#include "ns3/uinteger.h"
 using namespace std;
 using namespace ns3;
 
 #define TREE 0
 #define MESH 1
 #define MAX_CONNECTIONS 2
+
+//Things to do
+//1. Figure out how to implement the rest of the internet stack, Ip Address, Net Devices, Channels etc.(hint: I'm
+//   guessing object aggregators are used, chk 'em out!)
+//2. Create the Client application to send requests, join messages , create mesh connections and forward packets when required 
 
 //forward declaration
 class treeMeshNode;
@@ -19,6 +39,7 @@ struct Connection
     Ptr<treeMeshNode> Node;
     int type;
 };
+
 //An object to hold each node information
 class treeMeshNode: public Object
 {
@@ -38,16 +59,22 @@ class treeMeshNode: public Object
         }
 };
 
-//The server Implementation
+//The server Implementation - Modelled after the TOMO Paper found in the reference section of this project and
+//UdpEchoServer implementation provided along with ns3
 class tomoServer
 {
     private:
         Ptr<treeMeshNode> Server;
+        Ptr<Socket> m_socket;
+        uint32_t m_port;
+        Address m_local;
     public:
         tomoServer()
         {
             Server = CreateObject<treeMeshNode>();
             Server->Nodes = NULL;
+            m_socket = 0;
+            m_port = 1;
         }
         
         //Adds a client
@@ -89,6 +116,8 @@ class tomoServer
             }
             return false;
         }
+        
+        //just for testing the breadth first traversal logic
         void bf_traversal()
         {
             int i = 0;
@@ -107,6 +136,60 @@ class tomoServer
                     i++;
                 }
              }
+        }
+
+        void activateServer()
+        {
+            if(m_socket == 0)
+            {
+                //Bind any changes to m_socket to a call back function, like HandleRead and then chk for join and request
+                //messages on that
+                TypeId tid = TypeId::LookupByName("ns3::UdpSocketFactory");
+                m_socket = Socket::CreateSocket(Server->Nodes,tid);
+                InetSocketAddress local = InetSocketAddress(Ipv4Address::GetAny() , m_port);
+                m_socket->Bind(local);
+                if(addressUtils::IsMulticast(m_local))
+                {
+                    Ptr<UdpSocket> udpSocketx = DynamicCast<UdpSocket> (m_socket);
+                    if(udpSocketx)
+                    {
+                        udpSocketx->MulticastJoinGroup(0,m_local);
+                    }
+                    else
+                    {
+                        cout<<"Error";
+                    }
+                }
+            }
+            m_socket->SetRecvCallback(MakeCallback(&tomoServer::HandleRead, this));
+        }
+            
+        void HandleRead (Ptr<Socket> socket)
+        {
+            Ptr<Packet> packet;
+            Address from;
+            while (packet = socket->RecvFrom (from))
+            {
+                if (InetSocketAddress::IsMatchingType (from))
+                {
+                    NS_LOG_INFO ("Received " << packet->GetSize () << " bytes from " << InetSocketAddress::ConvertFrom (from).GetIpv4 ());
+                    packet->RemoveAllPacketTags ();
+                    packet->RemoveAllByteTags (); 
+                    NS_LOG_LOGIC ("Echoing packet");
+                    //socket->SendTo (packet, 0, from);
+                }
+            }
+         }
+
+
+        void deactivateServer()
+        {
+            //Remove the callback bindings on m_socket, actually equate the callback to a null callback
+            if (m_socket != 0)
+            {
+                m_socket->Close ();
+                m_socket->SetRecvCallback (MakeNullCallback<void, Ptr<Socket> > ());
+            }
         }
 
 };
